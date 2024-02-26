aed5b5d77e4f08218d8e4986c21a298a85341b05
j-r-neto <jose.r.neto@equatorialservicos.com.br>
2024-02-26 11:24:49 -0300

    :recycle: refactor: removendo funções desnecessárias

--- src/put-user-on-queue.js
+++ src/put-user-on-queue.js
@@ -1,19 +1,11 @@
 const authApiProxy = require('../proxies/authenticateApi');
-const getActiveUsers = require('./get-active-users');
-const filterUsersByDivision = require('./filter-users-by-division');
-const setUserPresence = require('./set-user-presence');
+const getActiveUsers = require('./functions/get-active-users');
+const filterUsersByDivision = require('./functions/filter-users-by-division');
+const setUserPresence = require('./functions/set-user-presence');
 
 const CLIENT_ID = "1b5d71fa-f9a8-463c-941f-07611e3ecf1b" ;
 const CLIENT_SECRET = "m90_wASq0rJYcykdGMFdQNwBeAds_iyoZkh7pCQkPRk";
 const ORG_REGION = "sa_east_1"; // eg. us_east_1
-
-// [
-//   'entities',   'pageSize',
-//   'pageNumber', 'total',
-//   'firstUri',   'nextUri',
-//   'lastUri',    'selfUri',
-//   'pageCount'
-// ]
 
 (async () => {
   const startTime = Date.now()
@@ -40,11 +32,12 @@ const ORG_REGION = "sa_east_1"; // eg. us_east_1
     pageNumber = users.pageNumber + 1
     pageCount = users.pageCount
   }
-
   
   const endTime = Date.now(); // Captura o tempo de término
   const executionTime = endTime - startTime; // Calcula o tempo de execução em milissegundos
   
-  console.log(`Qtd de pessoas fora da fila: ${nUsersOutOfQueue}`)
-  console.log(`Tempo de execução da RPA: ${executionTime}`)
+  console.log(`\nQtd de pessoas fora da fila: ${nUsersOutOfQueue}`)
+  console.log(`\nAgentes fora da fila:`)
+  console.log(`\nTempo de execução da RPA: ${executionTime}ms`)
+  console.log(`\nTempo de execução da RPA: ${executionTime/1000}s`)
 })()
